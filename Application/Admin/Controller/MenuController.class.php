707c3576a4e59b668ac2afc08b97ec7763f3dfe2
mumingv <mumingv@163.com>
2016-05-31 23:12:22 +0800

    5-1 后台入口文件优化和公共类的引入

---
+++ Application/Admin/Controller/MenuController.class.php
@@ -0,0 +1,29 @@
+<?php
+/***********************************************************
+ *     FileName: MenuController.class.php
+ *         Desc: 后台菜单相关处理
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2016-05-31 23:00:10
+ *      History:
+ ***********************************************************/
+namespace Admin\Controller;
+use Think\Controller;
+
+/**
+ * use Common\Model 这块可以不需要使用，框架默认会加载里面的内容
+ */
+class MenuController extends CommonController {
+
+    public function add(){
+        $this->display();
+        // echo "welcome to singcms";
+    }
+
+    public function index() {
+        $this->display();
+    }
+
+}
