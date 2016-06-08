2f54e7cdccc83bf9bd796ba0b44cd3865fbc1f90
mumingv <mumingv@163.com>
2016-06-08 09:18:37 +0800

    9-2 完成网站配置信息的填充和导航栏效果

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -1,9 +1,12 @@
-<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
+<?php if (!defined('THINK_PATH')) exit(); $config = D("Basic")->select(); $navs = D("Menu")->getBarMenus(); ?>
+<!doctype html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>sing资讯</title>
+  <title><?php echo ($config["title"]); ?></title>
   <link rel="stylesheet" href="/Public/css/bootstrap.min.css" type="text/css" />
+  <meta name="keywords" content="<?php echo ($config["keywords"]); ?>" />
+  <meta name="description" content="<?php echo ($config["description"]); ?>" />
   <link rel="stylesheet" href="/Public/css/home/main.css" type="text/css" />
 </head>
 <body>
@@ -11,16 +14,13 @@
   <div class="navbar-inverse">
     <div class="container">
       <div class="navbar-header">
-        <a href="">
+        <a href="/index.php">
           <img src="/Public/images/logo.png" alt="">
         </a>
       </div>
       <ul class="nav navbar-nav navbar-left">
-        <li><a href="" class="curr">首页</a></li>
-        <li><a href="">体育</a></li>
-        <li><a href="">科技</a></li>
-        <li><a href="">人文</a></li>
-        <li><a href="">汽车</a></li>
+          <li><a href="/index.php" <?php if($result['catid'] == 0): ?>class="curr"<?php endif; ?>>首页</a></li>
+        <?php if(is_array($navs)): foreach($navs as $key=>$vo): ?><li><a href="/index.php?c=cat&id=<?php echo ($vo["menu_id"]); ?>" <?php if($vo['menu_id'] == $result['catid']): ?>class="curr"<?php endif; ?>><?php echo ($vo["name"]); ?></a></li><?php endforeach; endif; ?>
       </ul>
     </div>
   </div>
