b68b8ede764b7acc2383a1f9112b93df28dc0fef
mumingv <mumingv@163.com>
2016-06-08 10:16:13 +0800

    9-3 完成首页焦点轮播图和推荐位数据填充

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,15 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        // 获取首页大图数据
+        $topPicNews = D("PositionContent")->select(array('status'=>1, 'position_id'=>2), 1);
+        // 首页3小图推荐
+        $topSmallNews = D("PositionContent")->select(array('status'=>1, 'position_id'=>3), 3);
+        
+        $this->assign('result', array(
+            'topPicNews' => $topPicNews,
+            'topSmallNews' => $topSmallNews,
+        ));
         $this->display();
     }
 }
